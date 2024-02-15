69ec9a9b8a717570295e4842ebd594181877a859
M ARIF SATRIA <arifsatria5554@gmail.com>
2024-02-15 16:02:44 +0700

    final verse

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -14,6 +14,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
